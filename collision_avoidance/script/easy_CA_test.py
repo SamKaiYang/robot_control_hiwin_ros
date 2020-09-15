adcbbac9fed90939548305d7ba4da83c4445c2b0
SamKaiYang <tt00621212@gmail.com>
2020-09-15 23:08:52 +0800

    easy_CA debug OK

---
+++ collision_avoidance/script/easy_CA_test.py
@@ -0,0 +1,119 @@
+import rospy
+import enum
+import time
+import numpy as np
+from control_node import HiwinRobotInterface
+from collision_avoidance.srv import collision_avoid, collision_avoidRequest
+
+pic_pos = [
+[5,  0.5,  -20, -180, 0, 0],
+[10, 0.5,  -20, -180, 0, 0],
+[15, 0.5,  -20, -180, 0, 0],
+[20, 0.5,  -20, -180, 0, 0],
+[25, 0.5,  -20, -180, 0, 0],
+[30, 0.5,  -20, -180, 0, 0],
+[39, 0.5,  -20, -180, 0, 0],
+[39, 10, -20, -180, 0, 0],
+[39, 15, -20, -180, 0, 0],
+[39, 20, -20, -180, 0, 0],
+[39, 25, -20, -180, 0, 0],
+[39, 30, -20, -180, 0, 0],
+[39, 35, -20, -180, 0, 0],
+[39, 40, -20, -180, 0, 0],
+[39, 45, -20, -180, 0, 0],
+[39, 50, -20, -180, 0, 0],
+[39, 58.2, -20, -180, 0, 0],
+[30, 58.2, -20, -180, 0, 0],
+[25, 58.2, -20, -180, 0, 0],
+[20, 58.2, -20, -180, 0, 0],
+[15, 58.2, -20, -180, 0, 0],
+[10, 58.2, -20, -180, 0, 0],
+[1.2, 58.2,  -20, -180, 0, 0],
+[1.2, 50,  -20, -180, 0, 0],
+[1.2, 45,  -20, -180, 0, 0],
+[1.2, 40,  -20, -180, 0, 0],
+[1.2, 35,  -20, -180, 0, 0],
+[1.2, 30,  -20, -180, 0, 0],
+[1.2, 25,  -20, -180, 0, 0],
+[1.2, 20,  -20, -180, 0, 0],
+[1.2, 15,  -20, -180, 0, 0],
+[1.2, 10,  -20, -180, 0, 0],
+[1.2, 5,   -20, -180, 0, 0]]
+
+class Arm_status(enum.IntEnum):
+    Idle = 1
+    Isbusy = 2
+
+class State(enum.IntEnum):
+    move = 0
+    take_pic = 1
+    finish = 2
+
+class EasyCATest:
+    def __init__(self):
+        self.arm_move = False
+        self.state = State.move
+        self.pos = np.array(pic_pos)
+
+    def easy_CA_client(self, req):
+        rospy.wait_for_service('/robot/easy_CA')
+        try:
+            hand_eye = rospy.ServiceProxy('/robot/easy_CA', collision_avoid)
+            res = hand_eye(req)
+            return res
+        except rospy.ServiceException as e:
+            print("Service call failed: %s"%e)
+
+    def Mission_Trigger(self):
+        if self.arm_move == True and robot_ctr.get_robot_motion_state() == Arm_status.Isbusy:
+            self.arm_move = False
+        # if Arm_state_flag == Arm_status.Idle and Sent_data_flag == 1:
+        if robot_ctr.get_robot_motion_state() == Arm_status.Idle and self.arm_move == False:
+            if self.state == State.move:
+                print('ffffffffffffffffffffffffffffffffffffffffffffffff')
+                pos = self.pos[0]
+                # position = [pos[0], pos[1], pos[2], pos[3], pos[4], pos[5]]
+                req = collision_avoidRequest()
+                req.ini_pose = pos
+                req.limit = 2
+                req.dis = 0
+                position = np.array(self.easy_CA_client(req).tar_pose)
+                print(position)
+                robot_ctr.Set_ptp_speed(30)
+                robot_ctr.Step_AbsLine_PosCmd(position)
+                self.pos = np.delete(self.pos, 0, 0)
+                self.state = State.take_pic
+                self.arm_move = True
+
+            if self.state == State.take_pic:
+                robot_ctr.Set_ptp_speed(80)
+                robot_ctr.Step_AbsPTPCmd([16.5, 29.2, 3, -180, 0, 0])
+                if len(self.pos) > 0:
+                    self.state = State.move
+                else:
+                    self.state = State.finish
+
+if __name__ == "__main__":
+    rospy.init_node('easy_CA_test')
+    robot_ctr = HiwinRobotInterface(robot_ip="192.168.0.1", connection_level=1,name="manipulator")
+    robot_ctr.connect()
+
+
+    robot_ctr.Set_operation_mode(0)
+    # set tool & base coor
+    tool_coor = [0,0,0,0,0,0]
+    base_coor = [0,0,0,0,0,0]
+    robot_ctr.Set_base_number(5)
+    # base_result = robot_ctr.Define_base(1,base_coor)
+    robot_ctr.Set_tool_number(15)
+    # tool_result = robot_ctr.Define_tool(1,tool_coor)
+    robot_ctr.Set_operation_mode(1)
+    robot_ctr.Set_override_ratio(20)
+    poses = []
+    strtage = EasyCATest()
+    while strtage.state != State.finish and not rospy.is_shutdown():
+        strtage.Mission_Trigger()
+        time.sleep(0.1)
+
+    for pose in poses:
+        print '{},'.format(pose)
--- collision_avoidance/src/collision_avoid.py
+++ collision_avoidance/src/collision_avoid.py
@@ -12,7 +12,7 @@ from collision_avoidance.srv import collision_avoid, collision_avoidResponse
 class CollisionAvoidance:
     def __init__(self, limit_array):
         self._hx, self._lx, self._hy, self._ly, self._lz = limit_array
-        self.limit = 0.1
+        self.limit = 0.085
         self.single_trans_angle = 5
         self._curr_pose = np.zeros(6)
         self._tool_coor = np.zeros(6)
@@ -56,7 +56,7 @@ class CollisionAvoidance:
         self._rtool_tool_trans[0:3, 3:] = np.mat([i/100 for i in self._tool_coor[:3]]).reshape(3, 1)
 
     def _check_position(self, limit):
-        xyz = [i for i in self._tar_trans[0:3, 3:]]
+        xyz = [i for i in np.array(self._tar_trans[0:3, 3:]).reshape(-1)]
         limits = [self._hx-limit, self._lx+limit, self._hy-limit, self._ly+limit, self._lz]
         xyz[0] = limits[0] if xyz[0] > limits[0] else xyz[0]
         xyz[0] = limits[1] if xyz[0] < limits[1] else xyz[0]
@@ -68,20 +68,21 @@ class CollisionAvoidance:
     def _check_collision(self):
         alarm = False
         base_rtool_trans = self._tar_trans * np.linalg.inv(self._rtool_tool_trans)
+        print(base_rtool_trans)
         xyz = [i for i in base_rtool_trans[0:3, 3:]]
         limits = [self._hx-self.limit, self._lx+self.limit, self._hy-self.limit, self._ly+self.limit]
 
         if xyz[0] > limits[0] or xyz[0] < limits[1]:
             direction = np.array(np.linalg.inv(self._tar_trans[0:3, 0:3])[0:3, 1:2]).reshape(-1) # get base Y axis in end trans
             angle = self.single_trans_angle if xyz[0] < limits[1] else -1 * self.single_trans_angle
-            trans_mat = np.mpt(tf.transformations.rotation_matrix(radians(angle), direction, point=None)) # use axis angle to get rotation
+            trans_mat = np.mat(tf.transformations.rotation_matrix(radians(angle), direction, point=None)) # use axis angle to get rotation
             self._tar_trans = self._tar_trans * trans_mat
             alarm = True
 
         if xyz[1] > limits[2] or xyz[1] < limits[3]:
             direction = np.array(np.linalg.inv(self._tar_trans[0:3, 0:3])[0:3, 0:1]).reshape(-1) # get base X axis in end trans
             angle = self.single_trans_angle if xyz[1] > limits[2] else -1 * self.single_trans_angle
-            trans_mat = np.mpt(tf.transformations.rotation_matrix(radians(angle), direction, point=None))
+            trans_mat = np.mat(tf.transformations.rotation_matrix(radians(angle), direction, point=None))
             self._tar_trans = self._tar_trans * trans_mat
             alarm = True
         return alarm
@@ -92,21 +93,26 @@ class CollisionAvoidance:
         self._ini_trans[0:3, 0:3] = tf.transformations.euler_matrix(abc[0], abc[1], abc[2], axes='sxyz')[0:3, 0:3]
         self._ini_trans[0:3, 3:] = np.mat([i/100 for i in req.ini_pose[:3]]).reshape(3, 1)
         self._tar_trans = copy.deepcopy(self._ini_trans)
+        print(self._tar_trans)
         limit = req.limit/100 if req.limit > 0 else 0
         self._check_position(limit if limit < 0.1 else 0.1)
-        for _ in range(10):
+        for i in range(10):
             if self._check_collision() is False:
+                print('fuckk: ', i)
                 break
         # make distance between target point and tool end base on tool Z axis
         mat = np.mat(np.identity(4))
         dis = req.dis/100 if req.dis is not None and req.dis > 0 else 0
-        mat[2, 2] = -dis if dis < 0.1 else -0.1
+        mat[2, 3] = -dis if dis < 0.1 else -0.1
+        print(self._tar_trans)
         self._tar_trans = self._tar_trans * mat
+        print(self._tar_trans)
 
         res = collision_avoidResponse()
         x, y, z = np.array(np.multiply(self._tar_trans[0:3, 3:], 100)).reshape(-1)
         a, b, c = [degrees(abc) for abc in tf.transformations.euler_from_matrix(self._tar_trans, axes='sxyz')]
         res.tar_pose = [x, y, z, a, b, c]
+        return res
 
     def __complete_collision_avoid(self, req):
         pass
