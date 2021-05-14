42f6177c5332fe92306b8595d6cc315042c71d2e
SamKaiYang <tt00621212@gmail.com>
2021-05-14 20:03:36 +0800

    huddle test ok 0514

--- huddle_mission/script/pick_and_place.py
+++ huddle_mission/script/pick_and_place.py
@@ -484,8 +484,8 @@ if __name__ == '__main__':
 
             robot_ctr.Set_operation_mode(1)
             
-            ArmGernel_Speed = 10
-            LineDown_Speed = 10
+            ArmGernel_Speed = 100
+            LineDown_Speed = 15
             robot_ctr.Set_override_ratio(ArmGernel_Speed)
 
             robot_ctr.Set_acc_dec_ratio(50)
